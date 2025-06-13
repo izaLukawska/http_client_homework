eafa80acdd49e3a9cafa4ecaed448623b1820c0e
iza.lukawska <lukawska.cf@gmail.com>
2025-06-13 19:01:08 +0200

    tasks for client comparison

---
+++ client_comparison/src/main/java/org/lukawska/client_comparison/ClientComparisonApplication.java
@@ -0,0 +1,13 @@
+package org.lukawska.client_comparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientComparisonApplication.class, args);
+	}
+
+}
