d791eed48bb950f7ff90f11132fdec8d95c362d4
iza.lukawska <lukawska.cf@gmail.com>
2025-06-28 13:50:19 +0200

    complete tests for UserService using MockWebServer

--- webClient_with_mockWebServer/src/test/java/org/lukawska/webclient_with_mockwebserver/service/UserServiceTest.java
+++ webClient_with_mockWebServer/src/test/java/org/lukawska/webclient_with_mockwebserver/service/UserServiceTest.java
@@ -1,25 +1,30 @@
 package org.lukawska.webclient_with_mockwebserver.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.lukawska.webclient_with_mockwebserver.dto.UserRequest;
+import org.lukawska.webclient_with_mockwebserver.dto.UserResponse;
 import org.lukawska.webclient_with_mockwebserver.exception.ClientException;
 import org.lukawska.webclient_with_mockwebserver.exception.ResourceNotFoundException;
 import org.lukawska.webclient_with_mockwebserver.exception.ServerException;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -27,6 +32,13 @@ public class UserServiceTest {
 	private MockWebServer mockWebServer;
 
 	private UserService userService;
+
+	private static ObjectMapper objectMapper;
+
+	@BeforeAll
+	static void setUpObjectMapper() {
+		objectMapper = new ObjectMapper();
+	}
 
 	@BeforeEach
 	void setUp() throws IOException {
@@ -48,13 +60,14 @@ public class UserServiceTest {
 	void shouldGetUserById() {
 		//given
 		Long id = 1L;
-		String responseBody = """
-				{
-				    "id": 1,
-				    "username": "test",
-				    "email": "test@test.com"
-				}
-				""";
+		UserResponse expectedResponse = new UserResponse(1L, "test", "test@test.com");
+		String responseBody;
+		try {
+			responseBody = objectMapper.writeValueAsString(expectedResponse);
+		} catch (JsonProcessingException e) {
+			fail("Failed to serialize UserResponse to JSON: " + e.getMessage());
+			return;
+		}
 
 		mockWebServer.enqueue(new MockResponse()
 				.setResponseCode(200)
@@ -67,14 +80,6 @@ public class UserServiceTest {
 				.expectNextMatches(user -> user.id().equals(1L) &&
 						user.username().equals("test"))
 				.verifyComplete();
-		try {
-			RecordedRequest request = mockWebServer.takeRequest();
-			assertThat(request.getPath()).isEqualTo("/users/1");
-			assertThat(request.getMethod()).isEqualTo("GET");
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			fail("Test interrupted");
-		}
 	}
 
 	@Test
@@ -83,20 +88,10 @@ public class UserServiceTest {
 		Long id = 1L;
 		mockWebServer.enqueue(new MockResponse().setResponseCode(404));
 
-		//when
+		//when && then
 		StepVerifier.create(userService.getUserById(id))
-				//then
 				.expectError(ResourceNotFoundException.class)
 				.verify();
-
-		try {
-			RecordedRequest recordedRequest = mockWebServer.takeRequest();
-			assertThat(recordedRequest.getPath()).isEqualTo("/users/" + id);
-			assertThat(recordedRequest.getMethod()).isEqualTo("GET");
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			fail("Test interrupted while waiting for request");
-		}
 	}
 
 	@Test
@@ -108,20 +103,10 @@ public class UserServiceTest {
 				.setResponseCode(status.value())
 				.setBody(status.getReasonPhrase()));
 
-		//when
+		//when && then
 		StepVerifier.create(userService.getUserById(id))
-				//then
 				.expectError(ClientException.class)
 				.verify();
-
-		try {
-			RecordedRequest recordedRequest = mockWebServer.takeRequest();
-			assertThat(recordedRequest.getPath()).isEqualTo("/users/" + id);
-			assertThat(recordedRequest.getMethod()).isEqualTo("GET");
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			fail("Test interrupted while waiting for request");
-		}
 	}
 
 	@Test
@@ -133,20 +118,165 @@ public class UserServiceTest {
 				.setResponseCode(status.value())
 				.setBody(status.getReasonPhrase()));
 
-		//when
+		//when && then
 		StepVerifier.create(userService.getUserById(id))
-				//then
+				.expectError(ServerException.class)
+				.verify();
+	}
+
+	@Test
+	void shouldReturnAllUsers_success() {
+		//given
+		List<UserResponse> expectedUserResponses = List.of(
+				new UserResponse(1L, "user1", "user1@example.com"),
+				new UserResponse(2L, "user2", "user2@example.com")
+		);
+		String responseBody = "";
+		try {
+			responseBody = objectMapper.writeValueAsString(expectedUserResponses);
+		} catch (JsonProcessingException e) {
+			fail("Failed to serialize List<UserResponse> to JSON: " + e.getMessage());
+		}
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(200)
+				.setHeader("Content-Type", "application/json")
+				.setBody(responseBody));
+
+		//when && then
+		StepVerifier.create(userService.getAllUsers())
+				.expectNextCount(2)
+				.verifyComplete();
+	}
+
+	@Test
+	void shouldThrowServerException_WhenGetAllUsers() {
+		//given
+		HttpStatus status = HttpStatus.BAD_GATEWAY;
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(status.value())
+				.setBody(status.getReasonPhrase()));
+
+		//when && then
+		StepVerifier.create(userService.getAllUsers())
 				.expectError(ServerException.class)
 				.verify();
 
+	}
+
+	@Test
+	void shouldCreateUser_success() {
+		//given
+		UserRequest request = new UserRequest("test1", "test@test.com");
+		UserResponse expectedResponse = new UserResponse(1L, "test1", "test@test.com");
+		String responseBody;
+		try {
+			responseBody = objectMapper.writeValueAsString(expectedResponse);
+		} catch (JsonProcessingException e) {
+			fail("Failed to serialize UserResponse to JSON: " + e.getMessage());
+			return;
+		}
+		mockWebServer.enqueue(new MockResponse()
+				.setHeader("Content-Type", "application/json")
+				.setResponseCode(201)
+				.setBody(responseBody));
+
+		//when && then
+		StepVerifier.create(userService.createUser(request))
+				.expectNextMatches(u -> u.username().equals("test1"))
+				.verifyComplete();
+	}
+
+	@Test
+	void shouldThrowClientException_WhenCreateUser() {
+		//given
+		UserRequest request = new UserRequest("test1", "test@test.com");
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(status.value())
+				.setBody(status.getReasonPhrase()));
+
+		//when && then
+		StepVerifier.create(userService.createUser(request))
+				.expectError(ClientException.class)
+				.verify();
+	}
+
+	@Test
+	void shouldThrowServerException_WhenCreateUser() {
+		//given
+		UserRequest request = new UserRequest("test1", "test@test.com");
+		HttpStatus status = HttpStatus.BAD_GATEWAY;
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(status.value())
+				.setBody(status.getReasonPhrase()));
+
+		//when && then
+		StepVerifier.create(userService.createUser(request))
+				.expectError(ServerException.class)
+				.verify();
+
+	}
+
+	@Test
+	void shouldDeleteUserById_success_withVerify(){
+		//given
+		Long id = 1L;
+		HttpStatus status = HttpStatus.NO_CONTENT;
+		mockWebServer.enqueue(new MockResponse().setResponseCode(status.value()));
+
+		//when && then
+		StepVerifier.create(userService.deleteUser(id))
+				.verifyComplete();
+
 		try {
 			RecordedRequest recordedRequest = mockWebServer.takeRequest();
-			assertThat(recordedRequest.getPath()).isEqualTo("/users/" + id);
-			assertThat(recordedRequest.getMethod()).isEqualTo("GET");
-
-		} catch (InterruptedException e) {
+			assertThat(recordedRequest.getPath()).isEqualTo("/users/1");
+			assertThat(recordedRequest.getMethod()).isEqualTo("DELETE");
+		} catch (InterruptedException e){
 			Thread.currentThread().interrupt();
-			fail("Test interrupted while waiting for request");
+			fail("Test interrupted");
 		}
+	}
+	@Test
+	void shouldThrowResourceNotFoundException_WhenDeleteUserById(){
+		//given
+		Long id = 999L;
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		mockWebServer.enqueue(new MockResponse().setResponseCode(status.value()));
+
+		//when
+		StepVerifier.create(userService.deleteUser(id))
+				.expectError(ResourceNotFoundException.class)
+				.verify();
+	}
+
+	@Test
+	void shouldThrowClientException_WhenDeleteUserById(){
+		//given
+		Long id = 999L;
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(status.value())
+				.setBody(status.getReasonPhrase()));
+
+		//when
+		StepVerifier.create(userService.deleteUser(id))
+				.expectError(ClientException.class).
+				verify();
+	}
+	@Test
+	void shouldThrowServerException_WhenDeleteUserById(){
+		//given
+		Long id = 999L;
+		HttpStatus status = HttpStatus.BAD_GATEWAY;
+		mockWebServer.enqueue(new MockResponse()
+				.setResponseCode(status.value())
+				.setBody(status.getReasonPhrase()));
+
+		//when
+		StepVerifier.create(userService.deleteUser(id))
+				.expectError(ServerException.class)
+				.verify();
+
 	}
 }
